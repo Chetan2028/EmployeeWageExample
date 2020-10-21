67a97f2b95c01aaf23b0d118a99f86d0334e8944
Chetan2028 <bmchetan2028@gmail.com>
2020-10-21 20:39:17 +0530

    [Chetan] Add . Added Program file to calculate monthly wages

--- Program.cs
+++ Program.cs
@@ -8,24 +8,70 @@ namespace EmployeeWage
         const int IS_FULL_TIME = 2;
         const int IS_PART_TIME = 1;
         const int EMPLOYEE_WAGE_PER_HOUR = 20;
+        const int NUMBER_OF_WORKING_DAYS = 20;
 
         ///Variables
         int employeeHours = 0;
+        int totalSalary = 0;
+
+        ///Create a reference of Random class
+        Random random = new Random();
+
+        public void CalculateMonthlyWages()
+        {
+            for (int index = 1; index <= NUMBER_OF_WORKING_DAYS; index++)
+            {
+                ///random function generates three values and checks the condition
+                if (random.Next(0, 3) == IS_FULL_TIME)
+                {
+                    employeeHours = 8;
+                }
+                else if (random.Next(0, 3) == IS_PART_TIME)
+                {
+                    employeeHours = 4;
+                }
+                else
+                {
+                    employeeHours = 0;
+                }
+                totalSalary += (employeeHours * EMPLOYEE_WAGE_PER_HOUR);
+            }
+            Console.WriteLine("Total Salary of Employees is : " + totalSalary);
+        }
+        /// <summary>
+        /// Calculates the wage using switch statement
+        /// </summary>
+        public void CalculateWageUsingSwitch()
+        {
+            switch (random.Next(0, 3))
+            {
+                case 2:
+                    Console.WriteLine("Employee is Full Time");
+                    employeeHours = 8;
+                    break;
+                case 1:
+                    Console.WriteLine("Employee is Part Time");
+                    employeeHours = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
+            }
+            Console.WriteLine("Employee Wage : " + employeeHours * EMPLOYEE_WAGE_PER_HOUR);
+        }
 
         /// <summary>
         /// Checks the employee presence and calculates the wage 
         /// </summary>
         public void EmployeeCheckAndWageCalculator()
         {
-            ///Create a reference of Random class
-            Random random = new Random();
             ///random function generates three values and checks the condition
-            if(random.Next(0,3) == IS_FULL_TIME)
+            if (random.Next(0, 3) == IS_FULL_TIME)
             {
                 employeeHours = 8;
                 Console.WriteLine("Employee is Full Time");
             }
-            else if(random.Next(0,3) == IS_PART_TIME)
+            else if (random.Next(0, 3) == IS_PART_TIME)
             {
                 employeeHours = 4;
                 Console.WriteLine("Employee is Part Time");
@@ -46,6 +92,10 @@ namespace EmployeeWage
             Program program = new Program();
             ///Call the function
             program.EmployeeCheckAndWageCalculator();
+            ///Call the function 
+            program.CalculateWageUsingSwitch();
+            ///Call the function to calculate monthly wage
+            program.CalculateMonthlyWages();
         }
     }
 }
