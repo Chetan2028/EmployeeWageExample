f5ad4da08146a0563c3ba7e7569a451c2ad0e976
Chetan Malagoudar <bmchetan2028@gmail.com>
2020-10-22 16:50:18 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,36 +4,10 @@ namespace EmployeeWage
 {
     class Program
     {
-        ///Constants
-        const int IS_PRESENT = 1;
-
-        /// <summary>
-        /// Checks the employee presence
-        /// </summary>
-        public void EmployeeCheck()
-        {
-            ///Create a reference of Random class
-            Random random = new Random();
-            ///random function generates two values and checks the condition
-            if(random.Next(2) == IS_PRESENT)
-            {
-                Console.WriteLine("Employee is Present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
-        }
-        /// <summary>
-        /// Defines the entry point of the application.
-        /// </summary>
-        /// <param name="args">The arguments.</param>
+        
         static void Main(string[] args)
         {
-            ///Create a reference of Program class
-            Program program = new Program();
-            ///Call the function
-            program.EmployeeCheck();
+            
         }
     }
 }
