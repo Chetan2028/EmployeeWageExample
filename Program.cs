06c35a286d3f2338103da042a5758e3a7f2b1e5c
Chetan2028 <bmchetan2028@gmail.com>
2020-10-22 17:18:45 +0530

    [Chetan] Add . Added Program file to compute employee wage

--- Program.cs
+++ Program.cs
@@ -8,38 +8,28 @@ namespace EmployeeWage
         const int IS_PRESENT = 1;
         const int EMPLOYEE_WAGE_PER_HOUR = 20;
 
-        ///Variables
-        int employeeHours = 0;
-
-        /// <summary>
-        /// Checks the employee presence and calculates the wage 
-        /// </summary>
-        public void EmployeeCheckAndWageCalculator()
-        {
-            ///Create a reference of Random class
-            Random random = new Random();
-            ///random function generates two values and checks the condition
-            if(random.Next(2) == IS_PRESENT)
-            {
-                employeeHours = 8;
-                Console.WriteLine("Employee is Present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
-            Console.WriteLine("Employee Wage : " + EMPLOYEE_WAGE_PER_HOUR * employeeHours);
-        }
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            ///Create a reference of Program class
-            Program program = new Program();
-            ///Call the function
-            program.EmployeeCheckAndWageCalculator();
+            ///Variables
+            int employeeHours = 0;
+
+            ///Create a reference of Random class
+            Random random = new Random();
+
+            ///Generates two random values and according to the condition assigns employee working hours
+            if (random.Next(0, 2) == IS_PRESENT)
+            {
+                employeeHours = 8;
+            }
+            else
+            {
+                employeeHours = 0;
+            }
+            Console.WriteLine("Employee Wage : " + EMPLOYEE_WAGE_PER_HOUR * employeeHours);
         }
     }
 }
