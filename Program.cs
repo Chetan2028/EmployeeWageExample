4ff4956a71080ef258674ef15f4765ad6185b233
Chetan2028 <bmchetan2028@gmail.com>
2020-10-22 17:28:50 +0530

    [Chetan] Add . Added Program file to compute wage using switch statement

--- Program.cs
+++ Program.cs
@@ -9,65 +9,32 @@ namespace EmployeeWage
         const int IS_PART_TIME = 1;
         const int EMPLOYEE_WAGE_PER_HOUR = 20;
 
-        ///Variables
-        int employeeHours = 0;
-
-        ///Create a reference of Random class
-        Random random = new Random();
-
-        /// <summary>
-        /// Calculates the employee wage using switch statement
-        /// </summary>
-        public void CalculateWageUsingSwitch()
-        {
-            switch (random.Next(0, 3))
-            {
-                case 2:
-                    Console.WriteLine("Employee is Full Time");
-                    employeeHours = 8;
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is Part Time");
-                    employeeHours = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    break;
-            }
-            Console.WriteLine("Employee Wage : " + employeeHours * EMPLOYEE_WAGE_PER_HOUR);
-        }
-        /// <summary>
-        /// Checks the employee presence and calculates the wage 
-        /// </summary>
-        public void EmployeeCheckAndWageCalculator()
-        {
-            ///random function generates three values and checks the condition
-            if(random.Next(0,3) == IS_FULL_TIME)
-            {
-                employeeHours = 8;
-                Console.WriteLine("Employee is Full Time");
-            }
-            else if(random.Next(0,3) == IS_PART_TIME)
-            {
-                employeeHours = 4;
-                Console.WriteLine("Employee is Part Time");
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
-            Console.WriteLine("Employee Wage : " + EMPLOYEE_WAGE_PER_HOUR * employeeHours);
-        }
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            ///Create a reference of Program class
-            Program program = new Program();
-            ///Call the function to calculate wage using switch statement
-            program.CalculateWageUsingSwitch();
+            ///Variables
+            int employeeHours = 0;
+
+            ///Create a reference of Random class
+            Random random = new Random();
+
+            ///Generates three random values and according to the condition assigns employee working hours using switchh statement
+            switch (random.Next(0, 3))
+            {
+                case IS_FULL_TIME:
+                    employeeHours = 8;
+                    break;
+                case IS_PART_TIME:
+                    employeeHours = 4;
+                    break;
+                default:
+                    employeeHours = 0;
+                    break;
+            }
+            Console.WriteLine("Employee Wage : " + EMPLOYEE_WAGE_PER_HOUR * employeeHours);
         }
     }
 }
