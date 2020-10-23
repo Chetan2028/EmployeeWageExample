65ed7bcedde2e4f14d0ca388232028bedf4c2f8f
Chetan2028 <bmchetan2028@gmail.com>
2020-10-23 19:01:38 +0530

    [Chetan] Add . Added employees total wage to linked list

---
+++ IComputeWage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    interface IComputeWage
+    {
+        public void AddCompany(string company, int empRatePerHour, int maxWorkingDays, int maxWorkingHours);
+
+        public void WageCalculation();
+    }
+}
